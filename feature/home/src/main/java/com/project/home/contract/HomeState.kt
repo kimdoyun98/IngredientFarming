eaf61815120e603fc72af8f68938c93d23f16d0e
kimdoyun98 <rladlsxo1250@gmail.com>
2026-02-24 00:29:30 +0900

    refactor: Immutable 어노테이션 추가

--- feature/home/src/main/java/com/project/home/contract/HomeState.kt
+++ feature/home/src/main/java/com/project/home/contract/HomeState.kt
@@ -1,9 +1,11 @@
 package com.project.home.contract
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class HomeState(
-    val ingredientCount: Int =0,
+    val ingredientCount: Int = 0,
     val expiresSoonCount: Int = 0,
     val recipeCount: Int = 0,
     val addStatus: Boolean = false,
 )
-
