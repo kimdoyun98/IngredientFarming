1dac002e2762ec25f9403cf2f03a6ee9214bc628
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-07 03:49:37 +0900

    refactor: OptIn 추가

--- core/ui/src/main/java/com/project/ui/IngredientCard.kt
+++ core/ui/src/main/java/com/project/ui/IngredientCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.SuggestionChipDefaults
@@ -103,6 +104,7 @@ fun IconIngredientCard(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun IngredientCardContent(
     item: Ingredient,
