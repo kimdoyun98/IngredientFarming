a7bc0da5ffdfa567a8600ef1212a5b9d73f838e9
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-07 02:43:22 +0900

    refactor: CategoryIconBox contentDescription stringResource로 변경

--- core/ui/src/main/java/com/project/ui/CategoryIconBox.kt
+++ core/ui/src/main/java/com/project/ui/CategoryIconBox.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.outlined.Spa
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.model.ingredient.IngredientCategory
@@ -101,7 +102,7 @@ internal fun CategoryIconBox(
                     IconResource.IsImageVector(Icons.Default.MoreHoriz)
                 }
             },
-            contentDescription = category.title,
+            contentDescription = stringResource(R.string.icon_description, category.title),
             tint = Color(category.color),
         )
     }
