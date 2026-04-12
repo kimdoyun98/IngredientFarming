d0d0dc0184c42c744fb0fca14d0246cb248d7590
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-12 18:58:09 +0900

    feat: 기존 Entity에 categoryGroup 컬럼 추가

--- core/database/src/main/java/com/project/database/model/IngredientEntity.kt
+++ core/database/src/main/java/com/project/database/model/IngredientEntity.kt
@@ -2,26 +2,53 @@ package com.project.database.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
-import com.project.model.ingredient.Ingredient
 import com.project.model.ingredient.IngredientCategory
-import com.project.model.ingredient.IngredientInfo
 import com.project.model.ingredient.IngredientStore
 
-@Entity(tableName = "IngredientEntity")
-data class IngredientEntity(
+@Entity(tableName = "IngredientCategoryGroupEntity")
+data class IngredientCategoryGroupEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String,
-    val category: IngredientCategory,
-    val store: IngredientStore,
-    @ColumnInfo(name = "hold_state") val holdState: Boolean
+    val groupType: String,
+    val category: IngredientCategory
 )
 
-fun IngredientEntity.asExternalModel(): IngredientInfo {
-    return IngredientInfo(
-        id = id,
-        name = name,
-        category = category,
-        store = store,
-    )
-}
+@Entity(
+    tableName = "IngredientEntity",
+    foreignKeys = [
+        ForeignKey(
+            entity = IngredientCategoryGroupEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["group_id"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ],
+    indices = [Index("group_id")]
+)
+data class IngredientEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+
+    val name: String,
+
+    val category: IngredientCategory,
+
+
+    val store: IngredientStore,
+
+    @ColumnInfo(name = "group_id")
+    val categoryGroupId: Int? = null,
+
+    @ColumnInfo(name = "hold_state")
+    val holdState: Boolean
+)
+
+//fun IngredientEntity.asExternalModel(): IngredientInfo {
+//    return IngredientInfo(
+//        id = id,
+//        name = name,
+//        category = category,
+//        store = store,
+//    )
+//}
