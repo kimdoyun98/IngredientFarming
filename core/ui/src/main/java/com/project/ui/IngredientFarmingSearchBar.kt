fbb2da31eceb8ca199555d80d877c9f13010618c
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-15 14:38:10 +0900

    fix: MVI TextField 글씨 깨짐 현상

--- core/ui/src/main/java/com/project/ui/IngredientFarmingSearchBar.kt
+++ core/ui/src/main/java/com/project/ui/IngredientFarmingSearchBar.kt
@@ -9,6 +9,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,12 +24,17 @@ fun IngredientFarmingSearchBar(
     onQueryChange: (String) -> Unit,
     onCloseClick: () -> Unit,
 ) {
+    var localText by remember { mutableStateOf(query) }
+
     OutlinedTextField(
         modifier = modifier
             .fillMaxWidth()
             .padding(4.dp),
-        value = query,
-        onValueChange = { newValue -> onQueryChange(newValue) },
+        value = localText,
+        onValueChange = { newValue ->
+            localText = newValue
+            onQueryChange(newValue)
+        },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
