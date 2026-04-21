cb6556c773c4680739e081a941806d5614ce5fe4
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-21 13:12:20 +0900

    refactor: Permisison 추상 클래스로 분리

--- core/permission/src/main/java/com/project/permission/IngredientFarmingPermission.kt
+++ core/permission/src/main/java/com/project/permission/IngredientFarmingPermission.kt
@@ -1,57 +1,13 @@
 package com.project.permission
 
-import android.Manifest
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
-import android.provider.Settings
 import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.ChecksSdkIntAtLeast
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Stable
-import androidx.core.content.ContextCompat
 import com.project.model.permission.PermissionState
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 @Stable
 class IngredientFarmingPermission(
-    private val activity: ComponentActivity
-) {
-    private val _cameraPermissionState = MutableStateFlow<PermissionState?>(null)
-    val cameraPermissionState = _cameraPermissionState.asStateFlow()
-
-    private val _mediaImagePermissionState =
-        MutableSharedFlow<PermissionState>(extraBufferCapacity = 1)
-    val mediaImagePermissionState = _mediaImagePermissionState.asSharedFlow()
-
-    private val launcher = activity.registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-
-        permissions.entries
-            .forEach { (permission, isGranted) ->
-
-                if (isGranted) updateMediaImagePermissionState(permission, PermissionState.Granted)
-
-                if (isLimitsPermission(activity, permission)) {
-                    updateMediaImagePermissionState(
-                        permission, PermissionState.PermanentlyDenied({
-                            openAppSettingsForPermission()
-                        })
-                    )
-                } else {
-                    updateMediaImagePermissionState(permission, PermissionState.Denied)
-                }
-
-            }
-    }
+    activity: ComponentActivity
+) : Permission(activity = activity) {
 
     fun launchMediaImagesPermission() {
         if (isTiramisuVersionHigher()) {
@@ -66,51 +22,10 @@ class IngredientFarmingPermission(
     }
 
     fun isGrantedCameraPermission(): Boolean {
-        return checkPermission(activity, CAMERA)
+        return checkPermission(CAMERA)
     }
 
     fun updateCameraPermissionState(state: PermissionState) {
-        _cameraPermissionState.value = state
-    }
-
-    private fun checkPermission(context: Context, permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(
-            context,
-            permission
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun openAppSettingsForPermission() {
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-            data = Uri.fromParts("package", activity.packageName, null)
-        }
-        activity.startActivity(intent)
-    }
-
-    private fun updateMediaImagePermissionState(permission: String, state: PermissionState) {
-        when (permission) {
-            CAMERA -> {
-                _cameraPermissionState.value = state
-            }
-
-            READ_MEDIA_IMAGES, READ_EXTERNAL_STORAGE -> {
-                _mediaImagePermissionState.tryEmit(state)
-            }
-        }
-    }
-
-    private fun isLimitsPermission(activity: Activity, permission: String): Boolean {
-        return !activity.shouldShowRequestPermissionRationale(permission)
-    }
-
-    companion object {
-        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-        private const val READ_MEDIA_IMAGES = Manifest.permission.READ_MEDIA_IMAGES
-        private const val READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE
-        private const val CAMERA = Manifest.permission.CAMERA
-
-        @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU)
-        private fun isTiramisuVersionHigher(): Boolean =
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+        updatePermissionState(permission = CAMERA, state = state)
     }
 }
---
+++ core/permission/src/main/java/com/project/permission/Permission.kt
@@ -0,0 +1,94 @@
+package com.project.permission
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.project.model.permission.PermissionState
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+abstract class Permission(
+    private val activity: ComponentActivity
+) {
+    private val _cameraPermissionState = MutableStateFlow<PermissionState?>(null)
+    val cameraPermissionState = _cameraPermissionState.asStateFlow()
+
+    private val _mediaImagePermissionState =
+        MutableSharedFlow<PermissionState>(extraBufferCapacity = 1)
+    val mediaImagePermissionState = _mediaImagePermissionState.asSharedFlow()
+
+    protected val launcher = activity.registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+
+        permissions.entries
+            .forEach { (permission, isGranted) ->
+
+                if (isGranted) updatePermissionState(permission, PermissionState.Granted)
+
+                if (isLimitsPermission(activity, permission)) {
+                    updatePermissionState(
+                        permission, PermissionState.PermanentlyDenied({
+                            openAppSettingsForPermission()
+                        })
+                    )
+                } else {
+                    updatePermissionState(permission, PermissionState.Denied)
+                }
+
+            }
+    }
+
+    protected fun updatePermissionState(permission: String, state: PermissionState) {
+        when (permission) {
+            CAMERA -> {
+                _cameraPermissionState.value = state
+            }
+
+            READ_MEDIA_IMAGES, READ_EXTERNAL_STORAGE -> {
+                _mediaImagePermissionState.tryEmit(state)
+            }
+        }
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU)
+    protected fun isTiramisuVersionHigher(): Boolean =
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+
+    protected fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            activity,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun isLimitsPermission(activity: Activity, permission: String): Boolean {
+        return !activity.shouldShowRequestPermissionRationale(permission)
+    }
+
+    private fun openAppSettingsForPermission() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", activity.packageName, null)
+        }
+        activity.startActivity(intent)
+    }
+
+    companion object {
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        const val READ_MEDIA_IMAGES = Manifest.permission.READ_MEDIA_IMAGES
+        const val READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE
+        const val CAMERA = Manifest.permission.CAMERA
+    }
+}
