3fa229d980f472f78ce2e228c63dc9e3a2bfe6cd
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-23 00:29:36 +0900

    refactor: count format unit 타입 변경

--- core/ui/src/main/java/com/project/ui/util/CountFormat.kt
+++ core/ui/src/main/java/com/project/ui/util/CountFormat.kt
@@ -1,12 +1,14 @@
 package com.project.ui.util
 
-fun Double.format(unit: String = "개"): String {
+import com.project.model.recipe.IngredientUnit
+
+fun Double.format(unit: IngredientUnit = IngredientUnit.COUNT): String {
     val intPart = this.toInt()
     val hasHalf = (this - intPart) >= 0.5
 
     return when {
-        intPart == 0 && hasHalf -> "반 $unit"
-        hasHalf -> "${intPart}$unit 반"
-        else -> "$intPart$unit"
+        intPart == 0 && hasHalf -> "반 ${unit.label}"
+        hasHalf -> "${intPart}${unit.label} 반"
+        else -> "$intPart${unit.label}"
     }
 }
