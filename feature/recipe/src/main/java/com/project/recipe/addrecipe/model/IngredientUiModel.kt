904602f86881281538fda530265abdfe18432a53
kimdoyun98 <rladlsxo1250@gmail.com>
2026-04-29 15:29:13 +0900

    refactor: UI 모델 Stable 처리

--- feature/recipe/src/main/java/com/project/recipe/addrecipe/model/IngredientUiModel.kt
+++ feature/recipe/src/main/java/com/project/recipe/addrecipe/model/IngredientUiModel.kt
@@ -1,7 +1,9 @@
 package com.project.recipe.addrecipe.model
 
+import androidx.compose.runtime.Stable
 import com.project.model.recipe.IngredientUnit
 
+@Stable
 data class IngredientUiModel(
     val id: Int,
     val name: String = "",
--- feature/recipe/src/main/java/com/project/recipe/addrecipe/model/RecipeStepUiModel.kt
+++ feature/recipe/src/main/java/com/project/recipe/addrecipe/model/RecipeStepUiModel.kt
@@ -1,5 +1,8 @@
 package com.project.recipe.addrecipe.model
 
+import androidx.compose.runtime.Stable
+
+@Stable
 data class RecipeStepUiModel(
     val id: Int,
     val description: String = "",
