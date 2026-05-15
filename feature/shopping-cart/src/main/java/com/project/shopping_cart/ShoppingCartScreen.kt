d4f6f734a2c3e7bd1990f90bcf03b49453b04ade
kimdoyun98 <rladlsxo1250@gmail.com>
2026-05-15 17:19:24 +0900

    feat: AppDefaultButton로 버튼 변경

--- feature/shopping-cart/src/main/java/com/project/shopping_cart/ShoppingCartScreen.kt
+++ feature/shopping-cart/src/main/java/com/project/shopping_cart/ShoppingCartScreen.kt
@@ -8,9 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -19,6 +16,7 @@ import androidx.compose.ui.graphics.Color.Companion.DarkGray
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.project.designsystem.component.AppDefaultButton
 import com.project.designsystem.theme.LightGreen
 import com.project.model.cart.ShoppingCartUiModel
 import com.project.model.ingredient.IngredientCategory
@@ -104,12 +102,11 @@ internal fun ShoppingCartScreen(
                     horizontalArrangement = Arrangement.End
                 ) {
                     if (saveSuccessItemState) {
-                        Button(
+                        AppDefaultButton(
                             modifier = Modifier
                                 .weight(1f)
                                 .padding(horizontal = 4.dp),
-                            colors = ButtonDefaults.buttonColors(containerColor = LightGreen),
-                            shape = RoundedCornerShape(8.dp),
+                            background = LightGreen,
                             onClick = onSaveSuccessItemsButtonClick,
                         ) {
                             Text(
