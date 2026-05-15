479f16ded9028db37779e3cbadacb81347cef2c9
kimdoyun98 <rladlsxo1250@gmail.com>
2026-05-15 15:06:02 +0900

    feat: ui-core 모듈 생성
    Modifier Extentions 관리

---
+++ core/ui-core/src/main/java/com/project/ui_core/modifier/LayoutModifier.kt
@@ -0,0 +1,56 @@
+package com.project.ui_core.modifier
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+fun Modifier.shadowLayout(
+    elevation: Int = 2
+) = this
+    .shadow(
+        elevation = elevation.dp,
+        shape = RoundedCornerShape(16.dp),
+        clip = false
+    )
+    .background(
+        color = White,
+        shape = RoundedCornerShape(16.dp)
+    )
+
+fun Modifier.dottedLineLayout(color: Color = LightGray) = this
+    .drawBehind {
+        val strokeWidth = 1.dp.toPx()
+        val dashWidth = 6.dp.toPx()
+        val gapWidth = 4.dp.toPx()
+
+        drawRoundRect(
+            color = color,
+            size = size,
+            style = Stroke(
+                width = strokeWidth,
+                pathEffect = PathEffect.dashPathEffect(
+                    floatArrayOf(dashWidth, gapWidth)
+                )
+            ),
+            cornerRadius = CornerRadius(16.dp.toPx())
+        )
+    }
+
+fun Modifier.circleLayout(size: Dp, background: Color) =
+    this
+        .size(size)
+        .clip(CircleShape)
+        .background(background)
